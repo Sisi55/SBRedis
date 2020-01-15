1385e6b1b3b51478e44e1484419bc31cf0de90ac
Sisi <dqgsh1055@gmail.com>
2020-01-15 23:37:32 +0900

    Initial commit

---
+++ src/main/java/com/example/javaredis/JavaredisApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaredisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaredisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javaredis/JavaredisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaredisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
